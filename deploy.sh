3cfad47197f579a6bab05eea1c6aedfe0f98a735
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 15:57:36 +0100

    Moves addition to known_hosts to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ echo "Deploying files to server"
 echo ssh ${SSH_USER} -p 18765
 # rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
 # ssh user@domain -p 18765
+echo ${SG_KEY} >> ${HOME}/.ssh/known_hosts
+less ${HOME}/.ssh/known_hosts
 ssh ${SSH_USER} -p 18765
 
 echo "Deployment complete"
