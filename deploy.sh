8b4e548004a2f1104515b76b15ffaccfbd05edef
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 15:21:16 +0100

    Adds real SSHUSER value and ssh command

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ echo "Deploying files to server"
 echo ssh ${SSH_USER} -p 18765
 # rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
 # ssh user@domain -p 18765
+ssh ${SSH_USER} -p 18765
 
 echo "Deployment complete"
