7a8a1f1f9ab1a5f5ef31341b2a1196866b9a00d9
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 16:58:42 +0100

    Removes comments from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 echo "Deploying files to server"
-# echo ssh ${SSH_USER} -p 18765
-# rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
-# ssh user@domain -p 18765
 echo "[${SG_DOMAIN}]:${SG_PORT} ecdsa-sha2-nistp256 ${SG_KEY}" >> ${HOME}/.ssh/known_hosts
-less ${HOME}/.ssh/known_hosts
 ssh ${SG_USER}@${SG_DOMAIN} -p ${SG_PORT}
 # rsync -avP -e 'ssh -p ${SG_PORT}' build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
-
 echo "Deployment complete"
