3096dd45830289c55e3de5840566fb42b2da2f53
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 13:48:31 +0100

    Adds Travis env to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-echo "Switching to branch master"
-# git checkout master
-
-echo "Building app"
-# npm run build
-
 echo "Deploying files to server"
+echo ${TRAVIS_REPO_SLUG}
 # rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
 # ssh user@domain -p 18765
 
