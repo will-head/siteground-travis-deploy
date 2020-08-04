61961923f73e7b1b32f0232825191646da9f7edb
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 17:25:59 +0100

    Changes ssh to rsync

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo "Deploying files to server"
 echo "[${SG_DOMAIN}]:${SG_PORT} ecdsa-sha2-nistp256 ${SG_KEY}" >> ${HOME}/.ssh/known_hosts
-ssh ${SG_USER}@${SG_DOMAIN} -p ${SG_PORT}
-# rsync -avP -e 'ssh -p ${SG_PORT}' build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
+# ssh ${SG_USER}@${SG_DOMAIN} -p ${SG_PORT}
+rsync -avP -e 'ssh -p ${SG_PORT}' build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
 echo "Deployment complete"
