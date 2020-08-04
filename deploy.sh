79a2327117d53b39a6b5b7bc0e7168d60ad5f4b6
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 16:20:48 +0100

    Adds quotes to SG_KEY and re-enbles add to known_hosts

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ echo "Deploying files to server"
 # echo ssh ${SSH_USER} -p 18765
 # rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
 # ssh user@domain -p 18765
-# echo ${SG_KEY} >> ${HOME}/.ssh/known_hosts
-# less ${HOME}/.ssh/known_hosts
-# ssh ${SSH_USER} -p 18765
-rsync -avP -e 'ssh -p 18765' build/ ${SSH_USER}:${SG_HOME}/domains/ppoc.dev/
+echo ${SG_KEY} >> ${HOME}/.ssh/known_hosts
+less ${HOME}/.ssh/known_hosts
+ssh ${SSH_USER} -p 18765
+# rsync -avP -e 'ssh -p 18765' build/ ${SSH_USER}:${SG_HOME}/domains/ppoc.dev/
 
 
 echo "Deployment complete"
