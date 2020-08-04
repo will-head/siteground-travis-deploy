445911a0210540263344847fe24e30cf97b95abe
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 17:28:37 +0100

    Changes single quotes to double around

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 echo "Deploying files to server"
 echo "[${SG_DOMAIN}]:${SG_PORT} ecdsa-sha2-nistp256 ${SG_KEY}" >> ${HOME}/.ssh/known_hosts
 # ssh ${SG_USER}@${SG_DOMAIN} -p ${SG_PORT}
-rsync -avP -e 'ssh -p ${SG_PORT}' build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
+rsync -avP -e "ssh -p ${SG_PORT}" build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
 echo "Deployment complete"
