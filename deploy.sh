8f54d0c80c09e8b4eea25d57f59c5d98a4191658
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 16:44:13 +0100

    Changes secure variables

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo "Deploying files to server"
 # echo ssh ${SSH_USER} -p 18765
 # rsync -avP -e 'ssh -p 18765' build/ user@domain:/path/
 # ssh user@domain -p 18765
-echo ${SG_KEY} >> ${HOME}/.ssh/known_hosts
+echo "[${SG_DOMAIN}]:${SG_PORT} ecdsa-sha2-nistp256 ${SG_KEY}" >> ${HOME}/.ssh/known_hosts
 less ${HOME}/.ssh/known_hosts
-ssh ${SSH_USER} -p 18765
+ssh ${SG_USER}@${SG_DOMAIN} -p 18765
 # rsync -avP -e 'ssh -p 18765' build/ ${SSH_USER}:${SG_HOME}/domains/ppoc.dev/
 
 
