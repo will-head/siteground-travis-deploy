bf28260e1705fbdd480d3961ffcb3dcc46256964
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-04 18:13:46 +0100

    Adds production argument to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if [[ "$1" = "production" ]] ; then
+  DEPLOY_DIR="ppoc.dev"
+else
+  DEPLOY_DIR="staging.ppoc.dev"
+fi
+
 echo "Deploying files to server"
 echo "[${SG_DOMAIN}]:${SG_PORT} ecdsa-sha2-nistp256 ${SG_KEY}" >> ${HOME}/.ssh/known_hosts
 # ssh ${SG_USER}@${SG_DOMAIN} -p ${SG_PORT}
-rsync -avP -e "ssh -p ${SG_PORT}" build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/ppoc.dev/
+rsync -avP -e "ssh -p ${SG_PORT}" build/ ${SG_USER}@${SG_DOMAIN}:/home/${SG_USER}/domains/${DEPLOY_DIR}/
 echo "Deployment complete"
